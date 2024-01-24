f23a97883feb6267747e042e28f615c5574b654d
DakshBaxi <baxidaksh2004@gmail.com>
2024-01-25 00:29:59 +0530

    footer completed

--- client/src/pages/Home page/Footer.jsx
+++ client/src/pages/Home page/Footer.jsx
@@ -10,7 +10,8 @@ const Footer = () => {
     Beiyo is a startup specializing in hostel <br /> accommodation. We partner with PG <br /> and hostel owners to renovate rooms <br /> and enhance facilities, offering a <br /> modern and comfortable living experience
     </p>
   </div>
-  <div className="footercompoents">
+ <div className="footercontent">
+ <div className="footercompoents">
     <ul>
       <li>About us</li>
       <li>Our Team</li>
@@ -24,17 +25,23 @@ const Footer = () => {
       <li>Cookie Policy</li>
     </ul>
   </div>
-  <div className="footercompoents">
+  <div className="ContactUsDiv">
   <ul>
-      <li>Contact Us</li>
-     <div className="socialmediadiv">
-     <p className='socilamedia' >Linkedin</p>
-      <p className='socilamedia' >Instagram</p>
-      <p className='socilamedia'>Twitter</p>
-      <p className='socilamedia'>Mail:- beiyo.work@gmail.com</p>
+     <div className="contactmaildiv">
+     <li className='contact'>Contact Us</li>
+      <p className='mail'>Mail:- beiyo.work@gmail.com</p>
      </div>
+     <div className="socialmediadiv">
+     <p className='socilamediaMobile' ><img src="/images/Linkedin.svg" alt="" /></p>
+      <p className='socilamediaMobile' ><img src="/images/Instagram.svg" alt="" /></p>
+      <p className='socilamediaMobile'><img src="/images/Twitter.svg" alt="" /></p>
+   
+     </div>
+   
+   
     </ul>
   </div>
+ </div>
   
 </div>
     <img className='randomfooterelem' src="/images/Ellipse 6.png" alt="" />
