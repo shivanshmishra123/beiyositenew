1a2da5fd2d8281d3d1a01fbc2de200f326374ee9
DakshBaxi <baxidaksh2004@gmail.com>
2024-03-19 23:51:36 +0530

    aboutpage done

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 import './About.css'
 const About = () => {
   return (
-    <div className='about page' >
-      <div className="contentabout">
-    <h1>We Didn't Find It For Us,
-      <br />
-     So We Created It For You</h1>
-    <p>
-    Stuck in cramped hostels or confined PGs, the options were far from ideal. Hence, Beiyo Living was born – a haven crafted by those who've walked in your shoes,
-    </p>
-
-   
-      </div>
-      <div className="imageabout">
-      <img src="/images/aboutus.png" alt="" />
-      </div>
-    </div>
+    <div>
+       <div className="AboutPage">
+   <div className="Heading">
+       <h1>Why We </h1>
+       <h2>are doing it !</h2>
+   </div>
+   <div className="PageArticle">
+       <div id="TextContent">
+           <p>IN oUR SEARCH</p>
+           <br/>
+           <p>WE CoULDN'T <span id="Letter">L</span>oCATE IT,</p>
+           <br/>
+           <p>So WE <span id="Letter">C</span>RAFTED IT FoR <span id="Letter">YOU.</span></p>
+       </div>
+       <div id="ImageContent">
+           <img className="AboutImage" src="./images/aboutuspng.png" alt="Team Image"/>
+       </div>
+   </div>
+   <div id="AboutContent">
+   Stuck in cramped hostels or confined PGs, the options were far from ideal. Hence, Beiyo Living was born – a haven crafted by those who've walked in your shoes,Stuck in cramped hostels or confined PGs, the options were far from ideal. Hence, Beiyo Living was born – a haven crafted by those who've walked in your shoes,Stuck in cramped hostels or confined PGs, the options were far from ideal. Hence, Beiyo Living was born – a haven crafted by those who've walked in your shoes,Stuck in cramped hostels or confined PGs, the options were far from ideal. Hence, Beiyo Living was born – a haven crafted by those who've walked in your shoes,Stuck in cramped hostels or confined PGs, the options were far from ideal. Hence, Beiyo Living was born – a haven crafted by those who've walked in your shoes,Stuck in cramped hostels or confined PGs, the options were far from ideal. Hence, Beiyo Living was born – a haven crafted by those who've walked in your shoes,Stuck in cramped hostels or confined PGs, the options were far from ideal. Hence, Beiyo Living was born – a haven crafted by those who've walked in your shoes,
+   </div>
+</div>
+</div>
   )
 }
 
