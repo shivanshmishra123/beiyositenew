abb79fc597a4eebc7b90a5dec56df37f2d283ce8
DakshBaxi <baxidaksh2004@gmail.com>
2024-05-23 14:11:00 +0530

    favicon added

---
+++ client/src/components/VirtualTour.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VirtualTour = () => {
+  return (
+    <div><a href="https://www.keypano.com/v/485_92p92a96k6-1716071682.html" >virtual tour</a></div>
+  )
+}
+
+export default VirtualTour
--- client/src/pages/HostelDetail.jsx
+++ client/src/pages/HostelDetail.jsx
@@ -14,6 +14,7 @@ import "swiper/css/navigation";
 import Form from '../components/Form';
 import { useParams } from 'react-router-dom';
 import HostelsComponent from '../components/HostelsComponent';
+import VirtualTour from '../components/VirtualTour';
 
 const HostelDetail = () => {
    const {id} = useParams();
@@ -164,6 +165,7 @@ const HostelDetail = () => {
               <p><img src="/images/location_Marker.svg" alt="" style={{height:"1rem"}} />{hostel&&hostel.nearby2}</p>
               <p><img src="/images/location_Marker.svg" alt="" style={{height:"1rem"}} />{hostel&&hostel.nearby3}</p>
           </div>
+          {/* <VirtualTour/> */}
          </div>
          {/* <div className="map">MAP</div> */}
           </div>
