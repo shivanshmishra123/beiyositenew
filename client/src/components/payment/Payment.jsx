df5be6ffb52948d2de754eb31e23812cd8348321
DakshBaxi <baxidaksh2004@gmail.com>
2024-06-26 13:08:38 +0530

    payment

--- client/src/components/payment/Payment.jsx
+++ client/src/components/payment/Payment.jsx
@@ -3,20 +3,25 @@ import React, { useState } from 'react';
 import { redirect } from 'react-router-dom';
 
 const Payment = () => {
-    const [amount, setAmount] = useState(0);
-    const [submiting,setSubmitting]=useState(false);
-    const handleSubmit = async e => {
+    const [amount, setAmount] = useState('');
+    const [submitting, setSubmitting] = useState(false);
+    const [initiate,setIntiate]=useState(false);
+    const [url,setUrl]=useState(null);
+    const handleSubmit = async (e) => {
         e.preventDefault();
         setSubmitting(true);
+        setIntiate(true);
         try {
-          const res = await axios.post('https://beiyositenew-api-alpha.vercel.app/api/pay/initiate', {amount: parseFloat(amount)});
-          console.log(res.data);
-          // Optionally, you can redirect the user here
+            const res = await axios.post('https://beiyositenew-api-alpha.vercel.app/api/pay/initiate', { amount: parseFloat(amount) });
+            console.log(res.data);
+            console.log(res.data.data.instrumentResponse.redirectInfo.url)
+            setUrl(res.data.data.instrumentResponse.redirectInfo.url);
+            // Optionally, you can redirect the user here
         } catch (err) {
-          console.error(err);
+            console.error(err);
         }
         setSubmitting(false);
-      };
+    };
 
     const handleChange = (e) => {
         setAmount(e.target.value);
@@ -24,10 +29,12 @@ const Payment = () => {
 
     return (
         <div style={{ paddingTop: '10rem' }}>
-          <form onSubmit={handleSubmit}>
-          <input type="text" onChange={handleChange} />
-          <button type="submit" disabled={submiting} onClick={handleSubmit}>Pay</button>
-          </form>
+            <form onSubmit={handleSubmit}>
+                <input type="number" value={amount} onChange={handleChange} />
+                <button type="submit" disabled={submitting}>Pay</button>
+            </form>
+            {initiate?(<div> <iframe href={url}/></div>):null}
+
         </div>
     );
 }
