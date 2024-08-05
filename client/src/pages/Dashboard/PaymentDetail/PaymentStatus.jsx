dbf17a456edc0a65a64c94003c4d5faf3c6f9b5e
DakshBaxi <baxidaksh2004@gmail.com>
2024-08-06 01:03:35 +0530

    cash one

--- client/src/pages/Dashboard/PaymentDetail/PaymentStatus.jsx
+++ client/src/pages/Dashboard/PaymentDetail/PaymentStatus.jsx
@@ -1,85 +1,13 @@
-// import React, { useContext, useEffect, useState } from 'react';
-// import axios from 'axios';
-// import { Box, Typography, CircularProgress, Button } from '@mui/material';
-// import AuthContext from '../../../context/AuthContext';
-// import {format} from 'date-fns'
-// import '../Dashboard.css';
-// const PaymentStatus = () => {
-//   const [payments, setPayments] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const { user} = useContext(AuthContext);
-//   const formatdate = (date) => {
-//     const d = new Date(date);
-
-//     // Custom format to include ordinal suffix
-//     const formattedDate = format(d, "do MMMM yyyy");
-
-//     return formattedDate;
-//   };
-//   useEffect(() => {
-//     const fetchPayments = async () => {
-//       try {
-//         const token = localStorage.getItem('token');
-//         const response = await axios.get(`https://beiyo-admin.vercel.app/api/dashboard/payments/${user._id}`, {
-//           headers: { Authorization: `Bearer ${token}` }
-//         });
-//         setPayments(response.data);
-//       } catch (error) {
-//         console.error('Error fetching payments:', error);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-//     fetchPayments();
-//   }, []);
-//   const handlePayment = async (id) => {
-//     const token = localStorage.getItem('token');
-//     const response = await axios.get(`https://beiyo-admin.vercel.app/api/dashboard/payment/${id}`, {
-//       headers: { Authorization: `Bearer ${token}` }
-//     });
-//     const amount = response.data.amount;
-//     console.log(amount);
-//     const paymentResponse = await axios.post('https://beiyo-admin.vercel.app/api/pay/initiate', {
-//       amount: amount, // Placeholder for the amount
-//     });
-//     const transactionId = paymentResponse.data.data.merchantTransactionId;
-//     window.location.href = paymentResponse.data.data.instrumentResponse.redirectInfo.url;
-//     localStorage.setItem('transactionId', transactionId);
-//     localStorage.setItem('amount', amount);
-//     localStorage.setItem('month', response.data.month);
-//     localStorage.setItem('userId', response.data.userId);
-//   }
-
-//   if (loading) return <CircularProgress />;
-
-//   return (
-//     <div className='paymentDetails'>
-//       <Typography variant="h4" gutterBottom>Payment Status</Typography>
-//       <Box className='paymentDetailstatusbox flex flex-col gap-2 ' >
-//       {payments.map((payment) => (
-//         <Box key={payment._id} sx={{  p: 2, border: '1px solid #ccc'}} className='h-100'>
-//           <Typography variant="body1">Amount: {payment.amount}</Typography>
-//           <Typography variant="body2">Date: {formatdate(payment.date)}</Typography>
-//           <Typography variant="body2">Status: {payment.status}</Typography>
-//           {payment.status==='due' && <Button onClick={()=>handlePayment(payment._id)}>Pay Now</Button>}
-//         </Box>
-//       ))}
-//       </Box>
-//     </div>
-//   );
-// };
-
-// export default PaymentStatus;
-
 
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Box, Typography, CircularProgress, Button, Collapse, IconButton } from '@mui/material';
 // import { ExpandMore, ExpandLess } from '@mui/icons-material';
 import AuthContext from '../../../context/AuthContext';
-import { format } from 'date-fns';
+import { add, format } from 'date-fns';
 import '../Dashboard.css';
 import { ChevronDownCircle, ChevronUpCircle } from 'lucide-react';
+import dayjs from 'dayjs';
 
 const PaymentStatus = () => {
   const [payments, setPayments] = useState([]);
@@ -93,7 +21,13 @@ const PaymentStatus = () => {
     const formattedDate = format(d, "do MMMM yyyy");
     return formattedDate;
   };
+  const today = dayjs();
+  const dayofMonth = today.date();
+  let additionalCharge = 0;
 
+  if (dayofMonth > 7) {
+    additionalCharge = (dayofMonth - 7) * 100;
+  }
   useEffect(() => {
     const fetchPayments = async () => {
       try {
@@ -111,7 +45,25 @@ const PaymentStatus = () => {
     fetchPayments();
   }, [user._id]);
 
-  const handlePayment = async (id) => {
+  const handleCurrentMonthPayment = async (id)=>{
+     console.log(additionalCharge);
+      const token = localStorage.getItem('token');
+     const response = await axios.get(`https://beiyo-admin.vercel.app/api/dashboard/payment/${id}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    const amount = response.data.amount;
+    const paymentResponse = await axios.post('https://beiyo-admin.vercel.app/api/pay/initiate', {
+      amount: amount+additionalCharge,
+    });
+    const transactionId = paymentResponse.data.data.merchantTransactionId;
+    window.location.href = paymentResponse.data.data.instrumentResponse.redirectInfo.url;
+    localStorage.setItem('transactionId', transactionId);
+    localStorage.setItem('amount', amount);
+    localStorage.setItem('month', response.data.month);
+    localStorage.setItem('userId', response.data.userId);
+  }
+
+  const handleFuturePayment = async (id) => {
     const token = localStorage.getItem('token');
     const response = await axios.get(`https://beiyo-admin.vercel.app/api/dashboard/payment/${id}`, {
       headers: { Authorization: `Bearer ${token}` }
@@ -175,10 +127,12 @@ const PaymentStatus = () => {
           <div key={payment._id}  className='h-fit p-2 flex justify-between items-center border-2 border-[#575756]'>
           <div className='flex flex-col gap-[0.2rem]'>
           <Typography variant="body1">Amount: {payment.amount}</Typography>
+          {additionalCharge!==0&&(<Typography>Additional charge: {additionalCharge}</Typography>)}
             <Typography variant="body2">Date: {formatDate(payment.date)}</Typography>
             <Typography variant="body2">Status: {payment.status}</Typography>
+            {payment.cash&&(<Typography variant="body2">Payment done by cash</Typography>)}
           </div>
-            {payment.status === 'due' && <button className='bg-[#f7d442] border-black border-2 rounded-full p-2' onClick={() => handlePayment(payment._id)}>Pay Now</button>}
+            {payment.status === 'due' && <button className='bg-[#f7d442] border-black border-2 rounded-full p-2' onClick={() => handleCurrentMonthPayment(payment._id)}>Pay Now</button>}
           </div>
         ))}
       </div>
@@ -199,8 +153,9 @@ const PaymentStatus = () => {
              <Typography variant="body1">Amount: {payment.amount}</Typography>
               <Typography variant="body2">Date: {formatDate(payment.date)}</Typography>
               <Typography variant="body2">Status: {payment.status}</Typography>
+              {payment.cash  &&(<Typography variant="body2">Payment done by cash</Typography>)}
              </div>
-              {payment.status === 'due' && <button className='bg-[#f7d442] border-black border-2 rounded-full p-2' onClick={() => handlePayment(payment._id)}>Pay Now</button>}
+              {payment.status === 'due' && <button className='bg-[#f7d442] border-black border-2 rounded-full p-2' onClick={() => handleFuturePayment(payment._id)}>Pay Now</button>}
             </div>
           ))}
          </div>
@@ -222,6 +177,7 @@ const PaymentStatus = () => {
             <Typography variant="body1">Amount: {payment.amount}</Typography>
               <Typography variant="body2">Date: {formatDate(payment.date)}</Typography>
               <Typography variant="body2">Status: {payment.status}</Typography>
+              {payment.cash===true &&(<Typography variant="body2">Payment done by cash</Typography>)}
             </div>
             </div>
           ))}
